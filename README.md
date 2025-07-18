44598eee0dbf22b575995915e31df997213d664a
ibra1410 <160061682+ibra1410@users.noreply.github.com>
2025-07-18 23:35:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# ytmp3-client
+
+Python client library for YT-MP3 API.
+
+## Install
+
+```
+pip install ytmp3-client
+```
+
+## Usage
+
+```python
+from ytmp3_client import YTtoMP3
+
+client = YTtoMP3()
+result = client.convert("rDeQr-DC4Xo")
+print(result)
+```
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ytmp3-client',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['requests'],
+    author='xibra_v',
+    description='Client library for YT-MP3 API',
+    url='https://github.com/username/ytmp3-client',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
