214c6d5317fde49c1e91cd3445f4b3eae0b7963b
ibra1410 <160061682+ibra1410@users.noreply.github.com>
2025-07-18 23:57:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ytmp3',
+    name='ytmp3-client',
     version='0.1.0',
     packages=find_packages(),
     install_requires=['requests'],
